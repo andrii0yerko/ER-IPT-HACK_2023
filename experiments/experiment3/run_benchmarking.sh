575d9efa27a9f7d9cef209b44bade655680dea8b
Andrii <aerko12@gmail.com>
2023-05-29 21:35:55 +0000

    more runs for e3/1

--- experiments/experiment3/run_benchmarking.sh
+++ experiments/experiment3/run_benchmarking.sh
@@ -7,7 +7,7 @@ CONFDIR="$( dirname "$0" )"
 cd $CONFDIR
 mkdir -p $DIRNAME
 
-for i in {1..3}
+for i in {1..10}
 do
     powertop --csv=$DIRNAME/$i"_powertop.txt" --workload=./workload.sh
     mv benchmark_summary.log $DIRNAME/$i"_benchmark.log"
